748c7914d23add80f8b4d2c0915e266493a979e7
solomon <ssbothwell@gmail.com>
2025-07-21 17:18:07 -0700

    Updates to unstable.

--- derivation.nix
+++ derivation.nix
@@ -5,4 +5,6 @@ buildPythonApplication {
   version = "1.0";
   propagatedBuildInputs = [ pulsectl ];
   src = ./.;
+  pyproject = true;
+  build-system = [ setuptools ];
 }
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Xob Volume Bar";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
